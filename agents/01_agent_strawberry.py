b2709d132a627aedbed41c6250c1e950481e7d61
Davide Eynard <davide.eynard@gmail.com>
2026-07-06 10:02:30 +0100

    Added agents scaffolding + 01_straberry

---
+++ agents/01_agent_strawberry.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from any_agent import AgentConfig, AnyAgent
+
+def count_character_occurrences(word: str, char: str) -> int:
+    """Count occurrences of a character in a word."""
+    return word.count(char)
+
+agent = AnyAgent.create(
+    "openai",
+    AgentConfig(
+        model_id="openai:gpt-5",
+        api_key=os.environ.get("OPENAI_API_KEY"),
+        # instructions="""You must use the available tools to find an answer.""",
+        # tools=[count_character_occurrences],
+        callbacks=[]
+    ),
+)
+
+char = 'r'
+string = "strawberry"
+# string = "strawrberrry"
+# string = "Llanfair­pwllgwyngyll­gogery­chwyrn­drobwll­llan­tysilio­gogo­goch"
+# string = "adsicnridsavubnsdrivubdsarridsbviusagrhadsifughrdsfiuhsda"
+print(f"String: {string}\nOccurrences of {char}: {string.count(char)}")
+
+prompt = sys.argv[1] if len(sys.argv) > 1 else f"How many times does the letter {char} occur in the word {string}?"
+print(f"{prompt}\n")
+
+for i in range(10):
+    agent_trace = agent.run(prompt)
+    print(f"Response #{i}: {agent_trace.final_output}")
---
+++ agents/README.md
@@ -0,0 +1,2 @@
+# TUMO agents 
+Some [any-agent](https://github.com/mozilla-ai/any-agent) examples for the TUMO Workshop "Own Your AI Stack"
